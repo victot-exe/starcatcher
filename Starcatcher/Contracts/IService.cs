2fa9b53a6ad2a5ed0dbc076a7b6bf14f61bc26f1
Victor <victorfarian@gmail.com>
2025-07-29 14:00:50 -0300

    Add CotaService & Interface

---
+++ Starcatcher/Contracts/IService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Starcatcher.Contracts
+{
+    public interface IService<E, I, O>
+    {
+        public E Create(I obj);
+
+        public List<E> GetAll();
+
+        public E GetById(O Id);
+
+        public E Update(O id, I obj);
+
+        public void Delete(O id);
+    }
+}
---
+++ Starcatcher/Services/CotaService.cs
@@ -0,0 +1,34 @@
+using Starcatcher.Contracts;
+using Starcatcher.DTOs;
+using Starcatcher.Entities;
+
+namespace Starcatcher.Services
+{
+    public class CotaService : IService<CotaDTOExit, CotaDTOEntry, int>
+    {
+        public CotaDTOExit Create(CotaDTOEntry obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CotaDTOExit> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CotaDTOExit GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CotaDTOExit Update(int id, CotaDTOEntry obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
