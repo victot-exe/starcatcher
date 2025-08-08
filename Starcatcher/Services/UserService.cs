40baea50564a698ac83083e1077a5d4498d15d57
Victor <victorfarian@gmail.com>
2025-08-08 10:43:46 -0300

    feat-Adicionando ao middleware a mensagem personalizada do erro de username unico

--- Starcatcher/Services/UserService.cs
+++ Starcatcher/Services/UserService.cs
@@ -2,6 +2,9 @@ using Starcatcher.Contracts;
 using Starcatcher.Entities;
 using Starcatcher.DTOs;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using Starcatcher.Exceptions;
 
 namespace Starcatcher.Services
 {
@@ -24,7 +27,14 @@ namespace Starcatcher.Services
             _validations.ExecuteAll(userDto);
             User user = new(userDto.Username, userDto.Password);
             user.Password = _passwordHasher.HashPassword(user, user.Password);
-            _repository.Create(user);
+            try
+            {
+                _repository.Create(user);
+            }
+            catch (DbUpdateException ex) when (IsUniqueConstraintViolation(ex))
+            {
+                throw new UsuarioJaExisteException(user.Username);
+            }
 
             return new(user);
         }
@@ -47,12 +57,20 @@ namespace Starcatcher.Services
 
         public UserExitDto Update(int id, UserEntryDto user)
         {
-            
+
             User entity = new(user.Username, user.Password);
-            
+
             entity.Password = _passwordHasher.HashPassword(entity, entity.Password);
 
             return new(_repository.Update(id, entity));
+        }
+
+        private bool IsUniqueConstraintViolation(DbUpdateException ex)
+        {
+            if (ex.InnerException is SqlException sqlEx)
+            return sqlEx.Number == 2601 || sqlEx.Number == 2627;
+
+            return false;
         }
     }
 }
