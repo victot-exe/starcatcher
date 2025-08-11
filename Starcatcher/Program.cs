d8468d3e5f6352539c0321182398d7b7592be357
Victor <victorfarian@gmail.com>
2025-08-11 11:57:37 -0300

    Fix update

--- Starcatcher/Program.cs
+++ Starcatcher/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.OpenApi.Models;
 using Starcatcher.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -53,50 +52,50 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)//Conf
 builder.Services.AddAuthorization();
 
 //TODO remover o swagger
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "StarcatcherAPI", Version = "v1" });
+// builder.Services.AddSwaggerGen(c =>
+// {
+//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "StarcatcherAPI", Version = "v1" });
 
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "Auth",
-        Description = "Insira o Token JWt no formato: Bearer {seu_token}",
-        In = ParameterLocation.Header, //TODO aqui eu troco pra por o cookie
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
+//     var securityScheme = new OpenApiSecurityScheme
+//     {
+//         Name = "Auth",
+//         Description = "Insira o Token JWt no formato: Bearer {seu_token}",
+//         In = ParameterLocation.Header, //TODO aqui eu troco pra por o cookie
+//         Type = SecuritySchemeType.Http,
+//         Scheme = "bearer",
+//         BearerFormat = "JWT",
+//         Reference = new OpenApiReference
+//         {
+//             Type = ReferenceType.SecurityScheme,
+//             Id = "Bearer"
+//         }
+//     };
 
-    c.AddSecurityDefinition("Bearer", securityScheme);
+//     c.AddSecurityDefinition("Bearer", securityScheme);
 
-    var securityRequirement = new OpenApiSecurityRequirement
-    {
-        {
-            securityScheme,
-            Array.Empty<string>()
-        }
-    };
-    c.AddSecurityRequirement(securityRequirement);
-});
+//     var securityRequirement = new OpenApiSecurityRequirement
+//     {
+//         {
+//             securityScheme,
+//             Array.Empty<string>()
+//         }
+//     };
+//     c.AddSecurityRequirement(securityRequirement);
+// });
 
 var app = builder.Build();
 
-app.MapGet("/", context =>
-{
-    context.Response.Redirect("/swagger");
-    return Task.CompletedTask;
-});
+// app.MapGet("/", context =>
+// {
+//     context.Response.Redirect("/swagger");
+//     return Task.CompletedTask;
+// });
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionMiddleware>();
