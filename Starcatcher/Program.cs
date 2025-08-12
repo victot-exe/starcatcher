4f8a8bcd95179e3ad8cf5d2831c16aeffab002c8
Victor <victorfarian@gmail.com>
2025-08-12 12:36:22 -0300

    adicionando cors

--- Starcatcher/Program.cs
+++ Starcatcher/Program.cs
@@ -51,51 +51,19 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)//Conf
 
 builder.Services.AddAuthorization();
 
-//TODO remover o swagger
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "StarcatcherAPI", Version = "v1" });
-
-//     var securityScheme = new OpenApiSecurityScheme
-//     {
-//         Name = "Auth",
-//         Description = "Insira o Token JWt no formato: Bearer {seu_token}",
-//         In = ParameterLocation.Header, //TODO aqui eu troco pra por o cookie
-//         Type = SecuritySchemeType.Http,
-//         Scheme = "bearer",
-//         BearerFormat = "JWT",
-//         Reference = new OpenApiReference
-//         {
-//             Type = ReferenceType.SecurityScheme,
-//             Id = "Bearer"
-//         }
-//     };
-
-//     c.AddSecurityDefinition("Bearer", securityScheme);
-
-//     var securityRequirement = new OpenApiSecurityRequirement
-//     {
-//         {
-//             securityScheme,
-//             Array.Empty<string>()
-//         }
-//     };
-//     c.AddSecurityRequirement(securityRequirement);
-// });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// app.MapGet("/", context =>
-// {
-//     context.Response.Redirect("/swagger");
-//     return Task.CompletedTask;
-// });
-
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionMiddleware>();
